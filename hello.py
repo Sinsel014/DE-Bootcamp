17ca01fffe847ece4c626928b103267deffdcd18
Rencelle Luat <wodiaml2@gmail.com>
2026-06-26 10:33:37 +0800

    Print hello

--- hello.py
+++ hello.py
@@ -1,12 +1,1 @@
-raw_name = " 4na CRUZ "
-clean_name = raw_name.strip().title()
-print(clean_name)
-
-temp = int(input("Reading temp is: "))
-
-if temp <= 36:
-    print("Temp is normal!")
-elif temp >=37:
-    print("Temp is hot!")
-while temp!=0:
-    break
+print("Hello, World!")
